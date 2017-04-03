3ecd5bd378789932214c676af39c3d6382592f22
000851496 <Aden.KLEEMANN@student.tafesa.edu.au>
2017-04-03 17:12:34 +0930

    frmAddModifyState code 99% done

--- VendorMaintenance/frmAddModifyState.cs
+++ VendorMaintenance/frmAddModifyState.cs
@@ -19,20 +19,72 @@ namespace VendorMaintenance
         public bool addState;
         public State state;
 
+        //Close window
         private void btnCancel_Click(object sender, EventArgs e)
         {
             //this.Hide();
             this.Close();
         }
 
+        //Accept changes
         private void btnAccept_Click(object sender, EventArgs e)
         {
-            //Accept changes/Add record
+            if (addState)
+            {
+                state = new State();
+                this.PutStateData(state);
+                DataContext.payables.States.InsertOnSubmit(state);
+
+                try
+                {
+                    DataContext.payables.SubmitChanges();
+                    this.DialogResult = DialogResult.OK;
+                    MessageBox.Show("Account code " + Convert.ToString(state) + " has been added to the state table");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, ex.GetType().ToString());
+                }
+            }
+            else
+            {
+                this.PutStateData(state);
+                try
+                {
+                    DataContext.payables.SubmitChanges();
+                    this.DialogResult = DialogResult.OK;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, ex.GetType().ToString());
+                }
+            }
+        }
+        
+
+        //Functions
+        private void PutStateData(State state)
+        {
+            state.StateCode = tbStateCode.Text;
+            state.StateName = tbStateName.Text;
+            state.FirstZipCode = Int32.Parse(tbZipCode.Text);
+            state.LastZipCode = Int32.Parse(tbZipCode2.Text);
         }
 
-        private void btnDelete_Click(object sender, EventArgs e)
+        private void frmAddModifyState_Load(object sender, EventArgs e)
         {
-            //Remove state data
+            if(addState != true)
+            {
+                this.DisplayStateData();
+            }
+        }
+
+        private void DisplayStateData()
+        {
+            tbStateCode.Text = state.StateCode.ToString();
+            tbStateName.Text = state.StateName.ToString();
+            tbZipCode.Text = state.FirstZipCode.ToString();
+            tbZipCode2.Text = state.LastZipCode.ToString();
         }
     }
 }
